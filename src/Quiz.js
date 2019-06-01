ad4bf714acc6be3c25a449c3d1d69cb008f374ac
GD <stefan.power@gmail.com>
2019-06-01 22:56:20 +0200

    beautifer added

--- src/Quiz.js
+++ src/Quiz.js
@@ -1,25 +1,24 @@
-import React, {
-  Component
-} from 'react'
+import React, {Component} from "react";
 
-let quizData = require('./quiz_data.json')
-
+let quizData = require("./quiz_data.json");
 
 class Quiz extends Component {
   constructor(props) {
-    super(props)
+    super(props);
     this.state = {
       quiz_position: 1
-    }
+    };
   }
 
   render() {
-    return ( <div >
-      <div className="QuizQuestion">{quizData.quiz_questions[0].instruction_text}
+    return (
+      <div>
+        <div className="QuizQuestion">
+          {quizData.quiz_questions[0].instruction_text}
+        </div>
       </div>
-      < /div >
-    )
+    );
   }
 }
 
-export default Quiz
+export default Quiz;
